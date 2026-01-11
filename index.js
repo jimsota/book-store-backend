99bbd81e217a460c3fe7cf88bad9c8367a1039fe
jimsota <jimsota30@gmail.com>
2026-01-11 20:39:39 +0000

    FIX CORS for Render frontend

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ const adminRoutes = require("./src/stats/admin.stats")
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ["http://localhost:5173",
+    "https://book-store-backend-a0hp.onrender.com"
+    ],
     credentials: true
 }))
 
